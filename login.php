d0914deb6923ff01c9051cf2d7a9ce3d3143803e
Gustavo Miguel <gustavo.biscaro@gmail.com>
2021-03-21 13:48:56 -0300

    2 files modified

--- login.php
+++ login.php
@@ -1,10 +1,45 @@
-﻿Página de Login
+﻿<?php
+session_start();
+if(isset($_POST['email']) && empty($_POST['email']) == false){
+    $email = addslashes($_POST['email']);
+    $senha = md5(addslashes($_POST['senha']));
 
-<form method="post">
-    E-mail: <br/>
-    <input type="email" name="email"> <br/><br/>
-    Senha: <br/>
-    <input type="password" name="senha"> <br/><br/>
+    $dsn = "mysql:dbname=blog;host=127.0.0.1";
+    $dbuser = "root";
+    $dbpass = "";
 
-    <input type="submit" value="Entrar">
+    try {
+        $db = new PDO($dsn, $dbuser, $dbpass);
+
+        $sql = $db->query("SELECT * FROM usuarios WHERE email = '$email' AND senha = '$senha'");
+
+        if($sql->rowCount() > 0) {
+
+            $dado = $sql->fetch();
+
+            $_SESSION['id'] =$dado['id'];
+
+            header("Location: index.php");
+        }
+
+    } catch(PDOException $e) {
+        echo "Falhou: ".$e->getMessage();
+    }
+}
+
+
+
+?>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+Pagina de login
+
+<form method="POST">
+
+    E-mail: <br />
+    <input type="email" name="email" /><br/><br/>
+    Senha: <br />
+    <input type="password" name="senha" /><br/><br/>
+
+    <input type="submit" value="Entrar" />
+
 </form>
