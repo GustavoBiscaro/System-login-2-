73b07304a7c2950290fd1f8bdb0754f0b907ea54
Gustavo Miguel <gustavo.biscaro@gmail.com>
2021-03-21 13:14:44 -0300

    Created the form and started the session

---
+++ login.php
@@ -0,0 +1,10 @@
+﻿Página de Login
+
+<form method="post">
+    E-mail: <br/>
+    <input type="email" name="email"> <br/><br/>
+    Senha: <br/>
+    <input type="password" name="senha"> <br/><br/>
+
+    <input type="submit" value="Entrar">
+</form>
